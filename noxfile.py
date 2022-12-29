0f4504b4e15c4f2bf6f396366bca17a03f884769
YuvalMilo <yuv.milo@gmail.com>
2022-12-29 16:26:43 +0200

    Adding Linting, Virtual environment control, and automated testing using nox

---
+++ noxfile.py
@@ -0,0 +1,16 @@
+import nox
+
+locations = ["src"]
+
+
+@nox.session(python=["3.10"])
+def lint(session):
+    args = session.posargs or locations
+    session.install(
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
+    session.run("flake8", *args)
