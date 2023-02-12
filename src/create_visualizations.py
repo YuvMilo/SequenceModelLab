860906b750e7959a87eb5e59d8a43c3710114ac8
YuvalMilo <yuv.milo@gmail.com>
2023-02-12 10:10:48 +0200

    Moved some bad code into backup

--- src/create_visualizations.py
+++
@@ -1,9 +0,0 @@
-from visualizations.vis_utils import vis_hippo_as_a_matrix,\
-    animate_SSM_state_dynamics, A_hippo_init_func, B_hippo_init_func
-
-if __name__ == "__main__":
-    #vis_hippo_as_a_matrix()
-    A = A_hippo_init_func(64)
-    B = B_hippo_init_func(64)
-    save_path = r"..\\results\\HippoStateDynamic2.mp4"
-    animate_SSM_state_dynamics(A, B, save_path=save_path,max_time=0.1)
